b1bc6bb188eb3a7c4abed59191738b0dc790cb47
degi100 <r.degering@mail.de>
2021-12-08 14:31:55 +0100

    prepare usecontext

--- src/pages/PageRegister.js
+++ src/pages/PageRegister.js
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+
 const PageRegister = () => {
-	return (
-		<>
-		This is the Register.
-		</>
-	)
-}
- 
+  const { siteStatus, toggleStatus } = useContext(AppContext);
+  return (
+    <div>
+      This is the Register.
+      <span className="highlight"> {siteStatus}</span>
+    </div>
+  );
+};
+
 export default PageRegister;
--- src/pages/PageWelcome.js
+++ src/pages/PageWelcome.js
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+
 const PageWelcome = () => {
-	return (
-		<>
-		This is the welcome page.
-		</>
-	)
-}
- 
+  const { siteStatus, toggleStatus } = useContext(AppContext);
+
+  return (
+    <div>
+      <p>
+        This is the welcome page. 
+		The current status is: 
+        <span className="highlight"> {siteStatus}</span>
+      </p>
+      <p>
+        <button onClick={toggleStatus}>Toggle Status</button>
+      </p>
+    </div>
+  );
+};
+
 export default PageWelcome;
